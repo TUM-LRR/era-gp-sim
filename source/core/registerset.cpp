888a421a68d1e7145de399aade5ccf0705cfd6f4
ProfPille <danield.riedel@tum.de>
2016-06-08 09:07:42 +0200

    updated formatting

--- source/core/registerset.cpp
+++ source/core/registerset.cpp
@@ -1,17 +1,19 @@
 #include "registerset.hpp"
 
 void RegisterSet::createRegister(std::string& name, int width){}
-void RegisterSet::aliasRegister (std::string& name,std::string& parent, int begin, int end){}
-void RegisterSet::aliasRegister (std::string& name,std::string& parent,int begin){}
+void RegisterSet::aliasRegister (std::string& name,std::string& parent,
+                                 int begin, int end){}
+void RegisterSet::aliasRegister (std::string& name,std::string& parent,
+                                 int begin){}
 
 MemoryValue RegisterSet::get(std::string& name)const{
-    return MemoryValue();
+  return MemoryValue();
 }
 
 void RegisterSet::put(std::string&, MemoryValue&){}
 MemoryValue RegisterSet::set(std::string& name, MemoryValue& value){
-    MemoryValue previousValue=get(name);
-    put(name,value);
-    return previousValue;
+  MemoryValue previousValue=get(name);
+  put(name,value);
+  return previousValue;
 }
 
