efc341697a0361d8155f464134dab03c6c911488
Erik Pohle <erik.pohle@mnet-mail.de>
2016-06-09 17:43:10 +0200

    Initial feature, defined factories for all node types

---
+++ include/arch/AbstractNodeFactories.hpp
@@ -0,0 +1,71 @@
+#ifndef ARCH_ABSTRACTNODEFACTORY_HPP
+#define ARCH_ABSTRACTNODEFACTORY_HPP
+
+#include <string>
+#include <vector>
+#include <memory>
+
+typedef std::string AbstractSyntaxTreeNode;
+
+class AbstractInstructionNodeFactory {
+public:
+  AbstractInstructionNodeFactory() {
+
+  }
+
+  virtual ~AbstractInstructionNodeFactory() = 0;
+
+  virtual std::shared_ptr<AbstractSyntaxTreeNode> createInstructionNode(const std::string& token) = 0;
+
+};
+
+class AbstractImmediateNodeFactory {
+public:
+  AbstractImmediateNodeFactory() {
+
+  }
+
+  virtual ~AbstractImmediateNodeFactory();
+
+  virtual std::shared_ptr<AbstractSyntaxTreeNode> createImmediateNode(const std::vector<bool> numericalValue) = 0;
+};
+
+class AbstractRegisterAccessNodeFactory {
+public:
+  AbstractRegisterAccessNodeFactory() {
+
+  }
+
+  virtual ~AbstractRegisterAccessNodeFactory() = 0;
+
+  virtual std::shared_ptr<AbstractSyntaxTreeNode> createRegisterAccessNode(const int registerAdress) = 0;
+};
+
+class AbstractMemoryAccessNodeFactory {
+public:
+  AbstractMemoryAccessNodeFactory() {
+
+  }
+
+  virtual ~AbstractMemoryAccessNodeFactory() = 0;
+
+  virtual std::shared_ptr<AbstractSyntaxTreeNode> createMemoryAccessNode() = 0;
+};
+
+class AbstractArithmeticOpNodeFactory {
+public:
+  static const int ADDITION = 1;
+  static const int SUBTRACTION = 2;
+  static const int MULTIPLICATION = 3;
+  static const int DIVISION = 4;
+
+  AbstractArithmeticOpNodeFactory() {
+
+  }
+
+  virtual ~AbstractArithmeticOpNodeFactory() = 0;
+
+  virtual std::shared_ptr<AbstractSyntaxTreeNode> createArithmeticOperationNode(const int opType) = 0;
+};
+
+#endif // ARCH_ABSTRACTNODEFACTORY_HPP
---
+++ source/arch/AbstractInstructionNodeFactory.cpp
@@ -0,0 +1,32 @@
+#include "include/arch/AbstractNodeFactories.hpp"
+
+
+// INSTRUCTION NODE
+
+AbstractInstructionNodeFactory::~AbstractInstructionNodeFactory() {
+
+}
+
+// IMMEDIATE NODE
+
+AbstractImmediateNodeFactory::~AbstractImmediateNodeFactory() {
+
+}
+
+// REGISTER ACCESS NODE
+
+AbstractRegisterAccessNodeFactory::~AbstractRegisterAccessNodeFactory() {
+
+}
+
+// MEMORY ACCESS NODE
+
+AbstractMemoryAccessNodeFactory::~AbstractMemoryAccessNodeFactory() {
+
+}
+
+// ARITHMETIC OPERATION NODE
+
+AbstractArithmeticOpNodeFactory::~AbstractArithmeticOpNodeFactory() {
+
+}
