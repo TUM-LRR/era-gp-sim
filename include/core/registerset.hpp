4fccb314138b79df1ffc52e2652972df2bc251a9
ProfPille <danield.riedel@tum.de>
2016-06-09 22:18:41 +0200

    implemented registers

--- include/core/registerset.hpp
+++ include/core/registerset.hpp
@@ -2,33 +2,44 @@
 #define ERAGPSIM_CORE_REGISTERSET_H
 
 #include <string>
+#include <unordered_map>
 
 #include "memoryvalue.hpp"
+#include "regp.hpp"
+#include "registervec.hpp"
 
+//a set of Registers and a map registername->register
 class RegisterSet{
 public:
   //creates a RegisterSet
-  RegisterSet()=default;
+  RegisterSet(int bucketCount=10);
   //destructor
   ~RegisterSet()=default;
 
   //creates a register in this RegisterSet with name 'name' and width 'width'
-  void createRegister(const std::string&, const int);
+  void createRegister(const std::string& name, const int width);
   //creates an alias for a register with the name 'name' to the bits
   //['begin';'end'[ of the register 'parent'
-  void aliasRegister (const std::string&,const std::string&, const int,
-                      const int);
+  void aliasRegister (const std::string& name,
+                      const std::string& parent,
+                      const int begin,
+                      const int end);
   //creates an alias for a register with the name 'name' to the bits
   //['begin';'parent'->end[ of the register 'parent'
-  void aliasRegister (const std::string&,const std::string&,const int=0);
+  void aliasRegister (const std::string& name,
+                      const std::string& parent,
+                      const int begin=0);
 
   //returns the value of the register with the name 'name'
-  MemoryValue get(const std::string&)const;
+  MemoryValue get(const std::string& name)const;
   //sets the value of the register with the name 'name'
-  void put(const std::string&,const  MemoryValue&);
+  void put(const std::string& name,const  MemoryValue& value);
   //sets the value of the register with the name 'name' and returns its former
   //value
-  inline MemoryValue set(const std::string&,const  MemoryValue&);
+  MemoryValue set(const std::string& name,const  MemoryValue& value);
+private:
+  std::unordered_map<std::string, RegisterID> _dict;
+  RegisterVec _registers;
 };
 
 #endif // ERAGPSIM_CORE_REGISTERSET_H
---
+++ include/core/registervec.hpp
@@ -0,0 +1,34 @@
+#ifndef ERAGPSIM_CORE_REGISTERVEC_H
+#define ERAGPSIM_CORE_REGISTERVEC_H
+
+#include <vector>
+
+#include "regp.hpp"
+#include "memoryvalue.hpp"
+#include "register.hpp"
+
+//a vector of registers that are not accessible by using std::strings
+class RegisterVec
+{
+public:
+  //creates an empty RegisterVec
+  RegisterVec();
+  //destructor
+  ~RegisterVec()=default;
+
+  //creates a register of width 'width'
+  int createRegister(const int width);
+
+  //returns the value of the register identified by 'id'
+  MemoryValue get(const RegisterID& id)const;
+  //changes the value of the register identified by 'id' to 'value'
+  void put(const RegisterID& id,const MemoryValue& value);
+  //changes the value of the register identified by 'id' to 'value' and returns
+  //the previous value
+  MemoryValue set(const RegisterID& id, const MemoryValue& value);
+private:
+  std::vector<Register> _data;
+  int _nextAdress=0;
+};
+
+#endif // ERAGPSIM_CORE_REGISTERVEC_H
