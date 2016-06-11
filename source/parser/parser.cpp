d234792436e7e0955e2d70feb608d664b22b4f78
kreudom <kreudom@gmail.com>
2016-06-11 18:26:37 +0200

    Added abstract Parser class #18

---
+++ source/parser/parser.cpp
@@ -0,0 +1,9 @@
+#include "../../include/parser/parser.hpp"
+
+std::string Parser::getSyntaxRegister(std::string name) {
+	return name;
+}
+
+std::string Parser::getSyntaxInstruction(std::string name) {
+	return name;
+}
