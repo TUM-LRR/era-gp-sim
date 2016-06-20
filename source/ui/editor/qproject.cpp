f8d8d7a57e3a228268404c8d5e2824d8fea989de
Jan Schopohl <schopohl@in.tum.de>
2016-06-20 17:56:03 +0200

    Added features to the editor
    New features are line numbers, cursor line highlighting, zoom and a prototype for errors

--- source/ui/editor/qproject.cpp
+++ source/ui/editor/qproject.cpp
@@ -1,5 +1,26 @@
+/*
+* C++ Assembler Interpreter
+* Copyright (C) 2016 Chair of Computer Architecture
+* at Technical University of Munich
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "qproject.hpp"
 #include <QRegularExpression>
+#include <QTextOption>
+#include <QFontMetrics>
 #include <iostream>
 #include <memory>
 
@@ -33,6 +54,12 @@ QProject::QProject(QObject *parent) : QObject(parent) {
 }
 
 void QProject::createHighlighter(QQuickTextDocument *qDocument) {
+    //set tab width to 4 spaces
+    QTextOption textOptions = qDocument->textDocument()->defaultTextOption();
+    QFontMetrics fontMetrics(qDocument->textDocument()->defaultFont());
+    textOptions.setTabStop(4*fontMetrics.width(' '));
+    qDocument->textDocument()->setDefaultTextOption(textOptions);
+
 	_highlighters.push_back(std::unique_ptr<SyntaxHighlighter>(
 			new SyntaxHighlighter(std::move(_keywords), qDocument->textDocument())));
 	std::cout << "Created new SyntaxHighlighter." << std::endl;
