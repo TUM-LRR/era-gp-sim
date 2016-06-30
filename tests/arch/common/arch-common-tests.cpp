02c0c565fee854543492bee914bab1f91a49922f
petergoldsborough <peter@goldsborough.me>
2016-06-30 20:52:05 +0200

    Preliminary commit to test travis

--- tests/arch/common/arch-common-tests.cpp
+++ tests/arch/common/arch-common-tests.cpp
@@ -17,7 +17,11 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <gtest/gtest.h>
 #include <iostream>
+#include <stdexcept>
+#include <string>
+#include <vector>
 
 #include "arch/common/architecture-formula.hpp"
 #include "arch/common/architecture.hpp"
@@ -28,28 +32,35 @@
 #include "arch/common/register-information.hpp"
 #include "arch/common/unit-information.hpp"
 
-auto main() -> int {
-  // clang-format off
-  auto eax =
-    RegisterInformation("eax")
-    .id(0)
-    .size(32)
-    .type(RegisterInformation::Type::INTEGER)
-    .hardwiredTo(5)
-    .addAlias("zero")
-    .enclosing(1)
-    .addConstituents({1, 2, 3});
-  auto unit = UnitInformation("cpu").addRegister(eax);
-  auto add = InstructionInformation("add").key(5);
-  auto set = InstructionSet({add});
-  auto base = ExtensionInformation("rvi32")
-    .alignmentBehavior(Information::AlignmentBehavior::STRICT)
-    .endianness(Information::Endianness::LITTLE)
-    .wordSize(32)
-    .addUnit(unit)
-    .addInstructions(set);
-  auto arch = Architecture("risc-v", base).validate();
-  // clang-format on
+TEST(arch_common_test, test_register_information) {
+  auto eax = RegisterInformation("r0")
+                 .id(0)
+                 .size(32)
+                 .type(RegisterInformation::Type::INTEGER)
+                 .constant(5)
+                 .addAlias("zero")
+                 .enclosing(1)
+                 .addConstituents({2, 3, 4});
 
-  std::cout << arch.getWordSize() << std::endl;
+  EXPECT_EQ(eax.getID(), 0);
+  EXPECT_TRUE(eax.hasSize());
+  EXPECT_EQ(eax.getSize(), 32);
+  EXPECT_EQ(eax.getType(), RegisterInformation::Type::INTEGER);
+  EXPECT_TRUE(eax.isConstant());
+  EXPECT_EQ(eax.getConstant<std::size_t>(), 5);
+  EXPECT_EQ(eax.getAliases(), std::vector<std::string>({"zero"}));
+  EXPECT_TRUE(eax.hasEnclosing());
+  EXPECT_EQ(eax.getEnclosing(), 1);
+  EXPECT_EQ(eax.getConstituents(), std::vector<std::size_t>({2, 3, 4}));
 }
+
+// auto unit = UnitInformation("cpu").addRegister(eax);
+// auto add = InstructionInformation("add").key(5);
+// auto set = InstructionSet({add});
+// auto base = ExtensionInformation("rvi32")
+//   .alignmentBehavior(Information::AlignmentBehavior::STRICT)
+//   .endianness(Information::Endianness::LITTLE)
+//   .wordSize(32)
+//   .addUnit(unit)
+//   .addInstructions(set);
+// auto arch = Architecture("risc-v", base).validate();
