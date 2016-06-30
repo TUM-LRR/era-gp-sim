fd22bf042d6c9d2986115b6ea63f89e92fe4e9e3
Erik Pohle <erik.pohle@mnet-mail.de>
2016-06-30 22:51:09 +0200

    #36 Fixed include

--- source/arch/common/abstract_node_factories.cpp
+++ source/arch/common/abstract_node_factories.cpp
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 
-#include "include/arch/common/abstract_node_factories.hpp"
+#include "arch/common/abstract-node-factories.hpp"
 
 /*
  * Stub implementations for the destructors
