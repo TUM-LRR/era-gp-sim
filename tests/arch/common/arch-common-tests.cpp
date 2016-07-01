6a37118e01f886a6ab97698d4beed3220fe4482c
petergoldsborough <peter@goldsborough.me>
2016-07-01 22:50:22 +0200

    Wrote tests for all information objects

--- tests/arch/common/arch-common-tests.cpp
+++ tests/arch/common/arch-common-tests.cpp
@@ -26,41 +26,104 @@
 
 #include "arch/common/architecture-formula.hpp"
 #include "arch/common/architecture.hpp"
+#include "arch/common/datatype-information.hpp"
 #include "arch/common/extension-information.hpp"
-#include "arch/common/information.hpp"
 #include "arch/common/instruction-information.hpp"
 #include "arch/common/instruction-key.hpp"
 #include "arch/common/instruction-set.hpp"
 #include "arch/common/register-information.hpp"
 #include "arch/common/unit-information.hpp"
 
-TEST(ArchCommonTest, TestRegisterInformation) {
-  auto eax = RegisterInformation("r0")
-                 .id(0)
-                 .size(32)
-                 .type(RegisterInformation::Type::INTEGER)
-                 .constant(5)
-                 .addAlias("zero")
-                 .enclosing(1)
-                 .addConstituents({2, 3, 4});
+struct ArchCommonTestFixture : ::testing::Test {
+  ArchCommonTestFixture() {
+    registerInformation.name("r0")
+        .id(0)
+        .size(32)
+        .type(RegisterInformation::Type::INTEGER)
+        .constant(5)
+        .addAlias("zero")
+        .enclosing(1)
+        .addConstituents({2, 3, 4});
 
-  EXPECT_EQ(eax.getID(), 0);
-  EXPECT_TRUE(eax.hasSize());
-  EXPECT_EQ(eax.getSize(), 32);
-  EXPECT_EQ(eax.getType(), RegisterInformation::Type::INTEGER);
-  EXPECT_TRUE(eax.isConstant());
-  EXPECT_EQ(eax.getConstant<std::size_t>(), 5);
-  EXPECT_EQ(eax.getAliases(), std::vector<std::string>({"zero"}));
-  EXPECT_TRUE(eax.hasEnclosing());
-  EXPECT_EQ(eax.getEnclosing(), 1);
-  EXPECT_EQ(eax.getConstituents(), std::vector<std::size_t>({2, 3, 4}));
+    unitInformation.name("cpu").addRegister(registerInformation);
+
+    instructionKey.addPair("opcode", 6).addPair("function", 9);
+    instructionInformation.mnemonic("add").key(instructionKey);
+
+    // clang-format off
+    instructionSet.addInstructions({
+        instructionInformation,
+        InstructionInformation{"mov", InstructionKey({{"opcode", 2}})}
+    });
+    // clang-format on
+
+    baseExtensionInformation.name("rvi32");
+
+    baseExtensionInformation.addInstructions(instructionSet);
+    baseExtensionInformation.addUnit(unitInformation);
+
+    baseExtensionInformation.wordSize(32);
+    baseExtensionInformation.endianness(
+        ArchitectureProperties::Endianness::MIXED);
+    baseExtensionInformation.alignmentBehavior(
+        ArchitectureProperties::AlignmentBehavior::STRICT);
+
+    // clang-format off
+    specialExtensionInformation.name("rva32");
+    specialExtensionInformation.addInstructions(InstructionSet({
+      {"lr", InstructionKey({{"opcode", 1}, {"function", 2}})},
+      {"sc", InstructionKey({{"opcode", 3}, {"function", 4}})}
+    }));
+    // clang-format on
+  }
+
+  InstructionKey instructionKey;
+  InstructionInformation instructionInformation;
+  InstructionSet instructionSet;
+  RegisterInformation registerInformation;
+  UnitInformation unitInformation;
+  ExtensionInformation baseExtensionInformation;
+  ExtensionInformation specialExtensionInformation;
+};
+
+TEST(ArchCommonTest, TestRegisterInformation) {
+  auto registerInformation = RegisterInformation()
+                                 .name("r0")
+                                 .id(0)
+                                 .type(RegisterInformation::Type::INTEGER)
+                                 .constant(5)
+                                 .addAlias("zero")
+                                 .enclosing(1)
+                                 .addConstituents({2, 3, 4});
+
+  EXPECT_FALSE(registerInformation.isValid());
+  registerInformation.size(32);
+  EXPECT_TRUE(registerInformation.isValid());
+
+  EXPECT_EQ(registerInformation.getID(), 0);
+  EXPECT_TRUE(registerInformation.hasSize());
+  EXPECT_EQ(registerInformation.getSize(), 32);
+  EXPECT_EQ(registerInformation.getType(), RegisterInformation::Type::INTEGER);
+  EXPECT_TRUE(registerInformation.isConstant());
+  EXPECT_EQ(registerInformation.getConstant<std::size_t>(), 5);
+  EXPECT_EQ(registerInformation.getAliases(),
+            std::vector<std::string>({"zero"}));
+  EXPECT_TRUE(registerInformation.hasEnclosing());
+  EXPECT_EQ(registerInformation.getEnclosing(), 1);
+  EXPECT_EQ(registerInformation.getConstituents(),
+            std::vector<std::size_t>({2, 3, 4}));
 }
 
 TEST(ArchCommonTest, TestInstructionKey) {
-  auto key = InstructionKey({{"opcode", 1}})
-                 .add("function", 2)
-                 .add({{"foo", 3}, {"bar", 4}})
-                 .add(std::unordered_map<std::string, std::size_t>{{"baz", 5}});
+  auto key =
+      InstructionKey()
+          .addPair("function", 2)
+          .addPairs({{"foo", 3}, {"bar", 4}})
+          .addPairs(std::unordered_map<std::string, std::size_t>{{"baz", 5}});
+
+  EXPECT_FALSE(key.isValid());
+  key.addPair("opcode", 1);
+  EXPECT_TRUE(key.isValid());
 
   EXPECT_TRUE(key.hasKey("opcode"));
   EXPECT_TRUE(key.hasKey("function"));
@@ -88,13 +151,132 @@ TEST(ArchCommonTest, TestInstructionKey) {
   EXPECT_EQ(key["baz"], 5);
 }
 
-// auto unit = UnitInformation("cpu").addRegister(eax);
-// auto add = InstructionInformation("add").key(5);
-// auto set = InstructionSet({add});
-// auto base = ExtensionInformation("rvi32")
-//   .alignmentBehavior(Information::AlignmentBehavior::STRICT)
-//   .endianness(Information::Endianness::LITTLE)
-//   .wordSize(32)
-//   .addUnit(unit)
-//   .addInstructions(set);
-// auto arch = Architecture("risc-v", base).validate();
+TEST(ArchCommonTest, TestDataTypeInformation) {
+  auto dataType = DataTypeInformation("word", 32);
+
+  EXPECT_TRUE(dataType.isValid());
+  EXPECT_EQ(dataType.getName(), "word");
+  EXPECT_EQ(dataType.getSize(), 32);
+}
+
+TEST(ArchCommonTest, TestArchitectureFormula) {
+  auto formula = ArchitectureFormula("riscv");
+
+  EXPECT_FALSE(formula.isValid());
+  formula.addExtension("rv32i").addExtension("rv32a");
+  EXPECT_TRUE(formula.isValid());
+
+  EXPECT_EQ(formula.getArchitectureName(), "riscv");
+
+  auto iterator = formula.cbegin();
+
+  EXPECT_EQ(*iterator++, "rv32i");
+  EXPECT_EQ(*iterator, "rv32a");
+}
+
+TEST(ArchCommonTest, TestInstructionInformation) {
+  auto instruction = InstructionInformation().mnemonic("add");
+
+  InstructionKey key({{"opcode", 6}, {"function", 9}});
+
+  EXPECT_FALSE(instruction.isValid());
+  instruction.key(key);
+  EXPECT_TRUE(instruction.isValid());
+
+  EXPECT_EQ(instruction.getMnemonic(), "add");
+  EXPECT_EQ(instruction.getKey(), key);
+}
+
+TEST_F(ArchCommonTestFixture, TestInstructionSet) {
+  InstructionSet set;
+
+  EXPECT_FALSE(set.isValid());
+  set.addInstruction(instructionInformation);
+  EXPECT_TRUE(set.isValid());
+}
+
+TEST_F(ArchCommonTestFixture, TestUnitInformation) {
+  auto unit = UnitInformation().name("cpu");
+
+  EXPECT_FALSE(unit.isValid());
+  unit.addRegister(registerInformation);
+  EXPECT_TRUE(unit.isValid());
+}
+
+TEST_F(ArchCommonTestFixture, TestExtensionInformation) {
+  auto extension = ExtensionInformation().name("rvi32");
+
+  EXPECT_FALSE(extension.isValid());
+  EXPECT_FALSE(extension.isValidBase());
+
+  extension.addInstructions(instructionSet);
+  extension.addUnit(unitInformation);
+
+  EXPECT_TRUE(extension.isValid());
+  EXPECT_FALSE(extension.isValidBase());
+
+  extension.wordSize(32);
+  extension.endianness(ArchitectureProperties::Endianness::MIXED);
+  extension.alignmentBehavior(
+      ArchitectureProperties::AlignmentBehavior::STRICT);
+
+  EXPECT_TRUE(extension.isValid());
+  EXPECT_TRUE(extension.isValidBase());
+
+
+  EXPECT_EQ(extension.getName(), "rvi32");
+  EXPECT_TRUE(Utility::isEqual(extension.getInstructions(), instructionSet));
+  EXPECT_EQ(extension.getUnits(),
+            std::vector<UnitInformation>{unitInformation});
+  EXPECT_EQ(extension.getWordSize(), 32);
+  EXPECT_EQ(extension.getEndianness(),
+            ArchitectureProperties::Endianness::MIXED);
+  EXPECT_EQ(extension.getAlignmentBehavior(),
+            ArchitectureProperties::AlignmentBehavior::STRICT);
+}
+
+TEST_F(ArchCommonTestFixture, TestExtensionInformationMerging) {
+  ASSERT_TRUE(baseExtensionInformation.isValidBase());
+  ASSERT_TRUE(specialExtensionInformation.isValid());
+
+  auto extension = baseExtensionInformation + specialExtensionInformation;
+
+  // Same as for the base-only test
+  EXPECT_EQ(extension.getName(), "rvi32");
+  EXPECT_EQ(extension.getUnits(),
+            std::vector<UnitInformation>{unitInformation});
+  EXPECT_EQ(extension.getWordSize(), 32);
+  EXPECT_EQ(extension.getEndianness(),
+            ArchitectureProperties::Endianness::MIXED);
+  EXPECT_EQ(extension.getAlignmentBehavior(),
+            ArchitectureProperties::AlignmentBehavior::STRICT);
+
+  // This should now include the new instructions
+  instructionSet += specialExtensionInformation.getInstructions();
+  EXPECT_TRUE(Utility::isEqual(extension.getInstructions(), instructionSet));
+}
+
+TEST_F(ArchCommonTestFixture, TestArchitecture) {
+  Architecture architecture("riscv");
+
+  EXPECT_FALSE(architecture.isValid());
+
+  architecture.extendBy(baseExtensionInformation);
+  architecture.extendBy(specialExtensionInformation);
+
+  EXPECT_TRUE(architecture.isValid());
+
+  architecture.validate();
+
+  EXPECT_EQ(architecture.getName(), "riscv");
+  EXPECT_EQ(architecture.getUnits(),
+            std::vector<UnitInformation>{unitInformation});
+  EXPECT_EQ(architecture.getWordSize(), 32);
+  EXPECT_EQ(architecture.getEndianness(),
+            ArchitectureProperties::Endianness::MIXED);
+  EXPECT_EQ(architecture.getAlignmentBehavior(),
+            ArchitectureProperties::AlignmentBehavior::STRICT);
+
+  instructionSet += specialExtensionInformation.getInstructions();
+  EXPECT_TRUE(Utility::isEqual(architecture.getInstructions(), instructionSet));
+}
