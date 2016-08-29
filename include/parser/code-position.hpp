5f38da60dfd30803470787638c9c382d67634ebb
DEXIS <david.schneller@tum.de>
2016-08-29 23:47:08 +0200

    New code position. #58

--- include/parser/code-position.hpp
+++ include/parser/code-position.hpp
@@ -22,10 +22,160 @@
 #include <utility>
 
 /**
- * \brief Denotes a code position in the format (line, character).
+ * \brief The coordinate component type for denoting positions in code. 
  */
-using CodePosition =
-    std::pair<unsigned int, unsigned int>;// For now. To be replaced by a real
-                                          // class, inheriting from pair.
+using CodeCoordinate = unsigned int;
+
+/**
+ * \brief A position in code.
+ */
+struct CodePosition
+{
+    /**
+     * \brief The x position in code (column/character).
+     */
+    CodeCoordinate x;
+
+    /**
+     * \brief The y position in code (row/line).
+     */
+    CodeCoordinate y;
+
+    /**
+     * \brief Creates a new CodePosition at (0,0).
+     */
+    CodePosition()
+        : CodePosition(0, 0)
+    {}
+
+    /**
+     * \brief Creates a new CodePosition with the given values (IMPORTANT: y before x coordinate! Because row is more important than column).
+     * \param iy The given y position.
+     * \param ix The given x position.
+     */
+    CodePosition(CodeCoordinate iy, CodeCoordinate ix)
+        : x(ix), y(iy)
+    {}
+
+    /**
+     * \brief Alias for the y position.
+     * \return The y position.
+     */
+    CodeCoordinate line() const
+    {
+        return y;
+    }
+
+    /**
+     * \brief Alias for the y position.
+     * \return The y position.
+     */
+    CodeCoordinate row() const
+    {
+        return y;
+    }
+
+    /**
+     * \brief Alias for the x position.
+     * \return The x position.
+     */
+    CodeCoordinate character() const
+    {
+        return x;
+    }
+
+    /**
+     * \brief Alias for the x position.
+     * \return The x position.
+     */
+    CodeCoordinate column() const
+    {
+        return x;
+    }
+
+    /**
+     * \brief Sums up two code position.
+     * \param a The first code position summand.
+     * \param b The second code position summand.
+     * \return The sum of the two code positions.
+     */
+    friend CodePosition operator + (const CodePosition& a, const CodePosition& b)
+    {
+        return CodePosition(a.y + b.y, a.x + b.x);
+    }
+
+    /**
+     * \brief Subtracts one code position from another.
+     * \param a The first code position to subtract from.
+     * \param b The second code position denoting the value to subtract.
+     * \return The difference between the first and the second code position.
+     */
+    friend CodePosition operator - (const CodePosition& a, const CodePosition& b)
+    {
+        return CodePosition(a.y - b.y, a.x - b.x);
+    }
+
+    /**
+     * \brief Moves a code position to the right by the given coordinate.
+     * \param a The code position to move.
+     * \param c The given coordinate.
+     * \return The transformed code position.
+     */
+    friend CodePosition operator >> (const CodePosition& a, CodeCoordinate c)
+    {
+        return a.moveRight(c);
+    }
+
+    /**
+     * \brief Moves a code position to the left by the given coordinate.
+     * \param a The code position to move.
+     * \param c The given coordinate.
+     * \return The transformed code position.
+     */
+    friend CodePosition operator << (const CodePosition& a, CodeCoordinate c)
+    {
+        return a.moveLeft(c);
+    }
+
+    /**
+     * \brief Moves a code position down by the given coordinate.
+     * \param c The given coordinate.
+     * \return The transformed code position.
+     */
+    CodePosition moveDown(CodeCoordinate c) const
+    {
+        return CodePosition(a.y + c, a.x);
+    }
+
+    /**
+     * \brief Moves a code position up by the given coordinate.
+     * \param c The given coordinate.
+     * \return The transformed code position.
+     */
+    CodePosition moveUp(CodeCoordinate c) const
+    {
+        return CodePosition(a.y - c, a.x);
+    }
+
+    /**
+     * \brief Moves a code position to the left by the given coordinate.
+     * \param c The given coordinate.
+     * \return The transformed code position.
+     */
+    CodePosition moveLeft(CodeCoordinate c) const
+    {
+        return CodePosition(a.y, a.x - c);
+    }
+
+    /**
+     * \brief Moves a code position to the right by the given coordinate.
+     * \param c The given coordinate.
+     * \return The transformed code position.
+     */
+    CodePosition moveRight(CodeCoordinate c) const
+    {
+        return CodePosition(a.y, a.x + c);
+    }
+};                                
 
 #endif
