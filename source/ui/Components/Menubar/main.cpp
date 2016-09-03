80397391b427ac39036e4d5a17c1993e8967e0b2
sabrie28 <sabine.rieder@tum.de>
2016-09-03 14:44:32 +0200

    First Version of the Model, Toolbar deleted

--- source/ui/Components/Menubar/main.cpp
+++ source/ui/Components/Menubar/main.cpp
@@ -1,12 +1,22 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <iostream>
+#include <QQMLContext>
+#include <QQmlEngine>
+
+#include "menubarmodel.hpp"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+
+    MenubarModel menubarmodel(engine.rootContext());
+
+    //engine.rootContext()->setContextProperty("menubarModel",  &menubarmodel);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+
 
     return app.exec();
 }
