ea1cec8a8f60266da83348c36d14fbfb378c104f
Erik Pohle <erik.pohle@mnet-mail.de>
2016-09-05 12:15:50 +0200

    #64 Added tests & stubs for mulw,div(u)w, rem(u)w

--- source/arch/riscv/instruction-node-factory.cpp
+++ source/arch/riscv/instruction-node-factory.cpp
@@ -135,6 +135,27 @@ void initializeIntegerInstructions(
         info, false);
   });
 }
+
+template<typename UnsignedWordSize, typename SignedWordSize>
+void initialize64BitWordInstructions(InstructionNodeFactory::InstructionMap& _instructionMap) {
+    _instructionMap.emplace("mulw", [](InstructionInformation& info) {
+      return std::make_unique<MultiplicationInstruction<UnsignedWordSize>>(
+          info, MultiplicationInstruction<UnsignedWordSize>::LOW,
+          MultiplicationInstruction<UnsignedWordSize>::SIGNED);
+    });
+    _instructionMap.emplace("divw", [](InstructionInformation& info) {
+        return std::make_unique<DivisionInstruction<UnsignedWordSize, SignedWordSize>>(info, true);
+    });
+    _instructionMap.emplace("divuw", [](InstructionInformation& info) {
+        return std::make_unique<DivisionInstruction<UnsignedWordSize, SignedWordSize>>(info, false);
+    });
+    _instructionMap.emplace("remw", [](InstructionInformation& info) {
+        return std::make_unique<RemainderInstruction<UnsignedWordSize, SignedWordSize>>(info, true);
+    });
+    _instructionMap.emplace("remuw", [](InstructionInformation& info) {
+        return std::make_unique<RemainderInstruction<UnsignedWordSize, SignedWordSize>>(info, true);
+    });
+}
 }
 
 void InstructionNodeFactory::initializeInstructionMap(
@@ -152,6 +173,7 @@ void InstructionNodeFactory::initializeInstructionMap(
     initializeIntegerInstructions<
         InstructionNodeFactory::RV64_integral_t,
         InstructionNodeFactory::RV64_signed_integral_t>(_instructionMap);
+    initialize64BitWordInstructions<InstructionNodeFactory::RV32_integral_t, InstructionNodeFactory::RV32_signed_integral_t>(_instructionMap);
   } else {
     // The given architecture does not define a valid word_size to create
     // IntegerInstructions
