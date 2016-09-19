46fb193d1cd0a5f918c4bd442728da3c06420261
Jan Schopohl <schopohl@in.tum.de>
2016-09-19 22:50:10 +0200

    Added missing documentation, improved formatting. #68

--- include/ui/ui.hpp
+++ include/ui/ui.hpp
@@ -20,50 +20,66 @@
 #ifndef ERAGPSIM_UI_UI_HPP_
 #define ERAGPSIM_UI_UI_HPP_
 
-#include <QObject>
-#include <QQuickItem>
 #include <QApplication>
-#include <QQmlContext>
+#include <QObject>
 #include <QQmlApplicationEngine>
-#include <QQmlEngine>
 #include <QQmlComponent>
-#include <vector>
+#include <QQmlContext>
+#include <QQmlEngine>
+#include <QQuickItem>
 #include <memory>
+#include <vector>
 
 #include "ui/qproject.hpp"
 
-class Ui : public QObject{
-Q_OBJECT
-public:
+/**
+ * This class creates the QmlEngine and starts the qml application.
+ *
+ * It is also used as global context property of the qml application in order to
+ * create new projects with the addProject method.
+ *
+ */
+class Ui : public QObject {
+  Q_OBJECT
+ public:
+  /**
+   * \brief Creates a new Ui object.
+   *
+   * Initializes the QmlApplication and the QmlEngine.
+   *
+   * \param argc argument count, passed by main function
+   * \param argv argument vector, passed by main function
+   */
+  Ui(int& argc, char** argv);
 
-    /**
-     * \brief Ui
-     * \param argc argument count, passed by main function
-     * \param argv argument vector, passed by main function
-     */
-    Ui(int& argc, char** argv);
+  /**
+   * Starts and runs the qml application
+   *
+   * This method is blocking, as it starts the message loop. It does only
+   * return when the application is stopped
+   *
+   * \return exit code of the application
+   */
+  int runUi();
 
-    /**
-     * Starts and runs the qml application
-     *
-     * This method is blocking, as it executes the message loop. It does only return when the application is stopped
-     *
-     * \return exit code of the application
-     */
-    int runUi();
+  /**
+   *  creates a new Project, is called from qml.
+   *
+   * \param tabItem The QQuickItem of the tab/parent of the project in qml. This
+   * is used as parent of the gui Project, therefore its lifetime is controlled
+   * by the tabItem.
+   * \param projectComponent The QQmlComponent to be used to create the qml part
+   * of the project.
+   */
+  Q_INVOKABLE void
+  addProject(QQuickItem* tabItem, QQmlComponent* projectComponent);
 
-    /**
-     *  creates a new Project
-     *
-     */
-    Q_INVOKABLE void addProject(QQuickItem* tabItem, QQmlComponent* projectComponent);
+ private:
+  /** The QApplication of this program. */
+  QApplication _qmlApplication;
 
-private:
-    QApplication _qmlApplication;
-    QQmlApplicationEngine _engine;
-
-
-
+  /** The QmlEngine of this program. */
+  QQmlApplicationEngine _engine;
 };
 
 #endif /* ERAGPSIM_UI_UI_HPP_ */
