cc4298076401ac858e8f295438c95f8cf1bd2360
Riedel Daniel <danield.riedel@tum.de>
2016-09-29 02:10:41 +0200

    conversions and conversion-wrapper

--- include/core/conversions.hpp
+++ include/core/conversions.hpp
@@ -20,7 +20,6 @@
 #define ERAGPSIM_CORE_ADVANCED_CONVERSIONS_HPP_
 
 #include <algorithm>
-#include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <functional>
@@ -28,9 +27,179 @@
 #include <vector>
 
 #include "core/memory-value.hpp"
+#include "common/assert.hpp"
 
-MemoryValue permute(const MemoryValue& memoryValue,
-                    const std::size_t byteSize,
-                    const std::vector<std::size_t> permutation);
+/**
+* TODO: Replace with version from memory implementation
+* Indicates the byte order of a memory value
+*/
+enum class Endianness {
+  BIG,
+  LITTLE
+};
+
+/**
+* TODO: Replace with version from architecture implementation
+* Indicates how signed numbers are represented
+*/
+enum class SignedRepresentation {
+  ONES_COMPLEMENT,
+  SIGN_BIT,
+  TWOS_COMPLEMENT
+};
+
+// Unsigned and little endian
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value, T>::type
+convert(const MemoryValue& memoryValue) {
+  T result = 0;
+  for (std::size_t i = memoryValue.internal().size(); i > 0; --i) {
+    result <<= 8;
+    result |= getByteAt(memoryValue, (i - 1) * 8);
+    // result |= memoryValue.getByteAt((i-1) * 8);
+  }
+  return result;
+}
+
+// little endian
+// true => negative
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value, T>::type
+convert(const MemoryValue& memoryValue, const std::function<bool(const MemoryValue&)> sgn, const std::function<MemoryValue(const MemoryValue&)> abs) {
+  T result = convert<T>(abs(memoryValue));
+  if (sgn(memoryValue) ^ (result < 0)) return 0 - result;
+  return result;
+}
+
+MemoryValue permute(const MemoryValue& memoryValue, const std::size_t byteSize, const std::vector<std::size_t> permutation);
+
+MemoryValue permute(const MemoryValue& memoryValue, const std::size_t byteSize, const std::function<std::size_t(std::size_t)> permutation);
+
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value, MemoryValue>::type
+convert(T value, const std::function<MemoryValue(const std::vector<std::uint8_t>&, std::size_t, bool)> sgn, std::size_t size) {
+  // T abs{ std::min(value) };
+  T abs{value};
+  if (abs < 0) abs = 0 - abs;
+  std::size_t sizeInByte{(size + 7) / 8};
+  std::vector<std::uint8_t> raw{};
+  for (std::size_t i = 0; i < sizeInByte; ++i) {
+    raw.push_back(static_cast<std::uint8_t>(abs & 0xFF));
+    abs >>= 8;
+  }
+  return sgn(raw, size, value < 0);
+}
+
+bool unSignumA(const MemoryValue& memoryValue);
+
+bool signumA(const MemoryValue& memoryValue);
+
+MemoryValue nonsignedB(const MemoryValue& memoryValue);
+
+MemoryValue signBitB(const MemoryValue& memoryValue);
+
+MemoryValue onesComplementB(const MemoryValue& memoryValue);
+
+MemoryValue twosComplementB(const MemoryValue& memoryValue);
+
+MemoryValue nonsignedC(const std::vector<std::uint8_t>& value, std::size_t size, bool sign);
+
+MemoryValue signBitC(const std::vector<std::uint8_t>& value, std::size_t size, bool sign);
+
+MemoryValue onesComplementC(const std::vector<std::uint8_t>& value, std::size_t size, bool sign);
+
+MemoryValue twosComplementC(const std::vector<std::uint8_t>& value, std::size_t size, bool sign);
+
+extern const std::function<bool(const MemoryValue&)> unSignum0;
+extern const std::function<bool(const MemoryValue&)> Signum0;
+
+extern const std::function<MemoryValue(const MemoryValue&)> nonsigned1;
+extern const std::function<MemoryValue(const MemoryValue&)> signBit1;
+extern const std::function<MemoryValue(const MemoryValue&)> onesComplement1;
+extern const std::function<MemoryValue(const MemoryValue&)> twosComplement1;
+
+extern const std::function<MemoryValue(const std::vector<std::uint8_t>&, std::size_t, bool)> nonsigned2;
+extern const std::function<MemoryValue(const std::vector<std::uint8_t>&, std::size_t, bool)> signBit2;
+extern const std::function<MemoryValue(const std::vector<std::uint8_t>&, std::size_t, bool)> onesComplement2;
+extern const std::function<MemoryValue(const std::vector<std::uint8_t>&, std::size_t, bool)> twosComplement2;
+
+namespace {
+class bigEndian {
+public:
+  bigEndian(std::size_t byteCount) :_byteCount{ byteCount } {}
+  bigEndian() :bigEndian(8) {};
+  bigEndian(const bigEndian&) = default;
+  bigEndian& operator=(const bigEndian&) = default;
+  bigEndian(bigEndian&&) = default;
+  bigEndian& operator=(bigEndian&&) = default;
+  std::size_t operator()(size_t x) {
+    return _byteCount - x;
+  }
+private:
+  std::size_t _byteCount;
+};
+}
+
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value, T>::type
+convert(const MemoryValue& memoryValue, SignedRepresentation representation=SignedRepresentation::TWOS_COMPLEMENT, std::size_t byteSize = 8, Endianness byteOrder=Endianness::LITTLE) {
+  MemoryValue permuted;
+  switch (byteOrder){
+  case Endianness::LITTLE:
+    permuted = memoryValue;
+    break;
+  case Endianness::BIG:
+    permuted = permute(memoryValue, byteSize, bigEndian{ memoryValue.getSize()/byteSize });
+    break;
+  default:
+    assert::that(false);
+  }
+  if (!std::is_signed(T)) {
+    return convert<T>(permuted, unSignum1, nonsigned1);
+  }
+  switch (representation){
+  case SignedRepresentation::SIGN_BIT:
+    return convert<T>(permuted, Signum0, signBit1);
+  case SignedRepresentation::ONES_COMPLEMENT:
+    return convert<T>(permuted, Signum0, onesComplement1);
+  case SignedRepresentation::TWOS_COMPLEMENT:
+    return convert<T>(permuted, Signum0, twosComplement1);
+  default:
+    assert::that(false);
+  }
+  return T{};
+}
+
+template <typename T>
+typename std::enable_if<std::is_integral<T>::value, MemoryValue>::type
+convert(T value, SignedRepresentation representation=SignedRepresentation::TWOS_COMPLEMENT, std::size_t byteSize=8, Endianness byteOrder=Endianness::LITTLE) {
+  MemoryValue converted;
+  if (!std::is_signed(T)) {
+    convert = convert<T>(value, nonsigned2, byteSize);
+  } else {
+    switch (representation) {
+    case SignedRepresentation::SIGN_BIT:
+      convert = convert<T>(value, signBit2, byteSize);
+      break;
+    case SignedRepresentation::ONES_COMPLEMENT:
+      convert = convert<T>(value, onesComplement2, byteSize);
+      break;
+    case SignedRepresentation::TWOS_COMPLEMENT:
+      convert = convert<T>(value, twosComplement2, byteSize);
+      break;
+    default:
+      assert::that(false);
+    }
+  }
+  switch (byteOrder) {
+  case Endianness::LITTLE:
+    return converted;
+  case Endianness::BIG:
+    return permute(converted, byteSize, bigEndian{ converted.getSize() / byteSize });
+  default:
+    assert::that(false);
+  }
+  return MemoryValue{};
+}
 
 #endif// ERAGPSIM_CORE_ADVANCED_CONVERSIONS_HPP_
--- source/core/conversions.cpp
+++ source/core/conversions.cpp
@@ -17,3 +17,185 @@
 */
 
 #include "core/conversions.hpp"
+
+namespace {
+constexpr uint8_t testEQ[8]{
+    0xFF, 0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3F, 0x7F,
+};
+}
+
+MemoryValue permute(const MemoryValue& memoryValue,
+                    const std::size_t byteSize,
+                    const std::vector<std::size_t> permutation) {
+  assert::that(byteSize > 0);
+  assert::that(memoryValue.getSize() % byteSize ==
+         0);// The value has to be made out of whole bytes
+  const std::size_t byteAmount{memoryValue.getSize() / byteSize};
+  assert::that(permutation.size() >=
+         byteAmount);// We need at least byteAmount values to permute
+
+  // permute the bytes
+  std::vector<std::uint8_t> permuted{};
+  std::uint8_t mask{testEQ[byteSize % 8]};
+  for (std::size_t i = 0; i < byteAmount; ++i) {
+    for (std::size_t j = 0; j < byteSize - 1; j += 8) {
+      if (j + 8 < byteSize) {
+        permuted.push_back(
+            memoryValue.getByteAt(permutation[i] * byteSize + j));
+      } else {
+        permuted.push_back(
+            memoryValue.getByteAt(permutation[i] * byteSize + j) & mask);
+      }
+    }
+  }
+
+  // pull together
+  if (byteSize % 8 == 0) {
+    // this is so much easier
+    return MemoryValue{permuted, permuted.size() * 8};
+  }
+
+  // Size of A Virtual Byte in byte
+  const std::size_t sizeOfByte{(byteSize + 7) / 8};
+  // The raw result vector
+  std::vector<std::uint8_t> raw{};
+  // The number of bits in the buffer
+  std::size_t bitsInBuffer{0};
+  // the Buffer
+  std::uint8_t bufferByte{0};
+
+  // for each byte
+  for (std::size_t i = 0; i < byteAmount; ++i) {
+    for (std::size_t j = 0; j < sizeOfByte; ++j) {
+      // take byte from permuted vector and put into buffer
+      bufferByte |= permuted[i * sizeOfByte + j] << bitsInBuffer;
+      // Add size of this byte to bitsInBuffer
+      if (j + 1 < sizeOfByte) {
+        // inner byte => full byte
+        bitsInBuffer += 8;
+      } else {
+        // last byte in virtual byte
+        bitsInBuffer += byteSize % 8;
+      }
+      // Buffer Filled
+      if (bitsInBuffer >= 8) {
+        // push onto result vector
+        raw.push_back(bufferByte);
+        // the part of the current byte that didn'tfit into the Buffer become
+        // the new Buffer
+        bufferByte = permuted[i * sizeOfByte + j] >>
+                     (16 - bitsInBuffer);//(8 - (bIB % 8))
+        bitsInBuffer -= 8;
+      }
+    }
+  }
+  // push final Byte
+  raw.push_back(bufferByte);
+  return MemoryValue(raw, memoryValue.getSize());
+}
+
+MemoryValue permute(const MemoryValue& memoryValue, const std::size_t byteSize, const std::function<std::size_t(std::size_t)> permutation) {
+  assert::that(byteSize > 0);
+  assert::that(memoryValue.getSize() % byteSize == 0);// The value has to be made out of whole bytes
+  std::vector<std::size_t> permutationVector{};
+  std::size_t byteAmount{memoryValue.getSize() / byteSize};
+  for (std::size_t i = 0; i < byteAmount; ++i) {
+    permutationVector.push_back(permutation(i));
+  }
+  return permute(memoryValue, byteSize, permutationVector);
+}
+
+bool unSignumA(const MemoryValue& memoryValue) {
+  return false;
+}
+
+bool signumA(const MemoryValue& memoryValue) {
+  return memoryValue.get(memoryValue.getSize() - 1);
+}
+
+MemoryValue nonsignedB(const MemoryValue& memoryValue) {
+  return memoryValue;
+}
+
+MemoryValue signBitB(const MemoryValue& memoryValue) {
+  assert::that(memoryValue.getSize() > 1);
+  return memoryValue.subSet(0, memoryValue.getSize() - 1);
+}
+
+MemoryValue onesComplementB(const MemoryValue& memoryValue) {
+  assert::that(memoryValue.getSize() > 1);
+  std::vector<std::uint8_t> raw{memoryValue.internal()};
+  // cut last byte if necessary
+  if (memoryValue.getSize() % 8 == 1) {
+    raw.pop_back();
+  }
+  // invert all bits
+  for (std::size_t i = 0; i < raw.size(); ++i) {
+    raw[i] ^= 0xFF;
+  }
+  return MemoryValue{raw, memoryValue.getSize() - 1};
+}
+
+MemoryValue twosComplementB(const MemoryValue& memoryValue) {
+  // invert
+  MemoryValue result{onesComplementB(memoryValue)};
+  // add one
+  for (std::size_t i = 0; i < result.getSize() - 1; ++i) {
+    if (!result.flip(i)) return result;
+  }
+  return result;
+}
+
+MemoryValue nonsignedC(const std::vector<std::uint8_t>& value,
+                       std::size_t size,
+                       bool sign) {
+  assert::that(!sign);// don't convert signed values via nonsigned
+  return MemoryValue{value, size};
+}
+
+MemoryValue
+signBitC(const std::vector<std::uint8_t>& value, std::size_t size, bool sign) {
+  MemoryValue result{value, size};
+  // set the sign bit
+  result.set(size - 1, sign);
+  return result;
+}
+
+MemoryValue onesComplementC(const std::vector<std::uint8_t>& value,
+                            std::size_t size,
+                            bool sign) {
+  std::vector<std::uint8_t> raw{value};
+  // invert all bits
+  for (std::size_t i = 0; i < raw.size(); ++i) {
+    raw[i] ^= 0xFF;
+  }
+  MemoryValue result{std::move(raw), size};
+  // set the sign bit
+  result.set(size - 1, sign);
+  return result;
+}
+
+MemoryValue twosComplementC(const std::vector<std::uint8_t>& value,
+                            std::size_t size,
+                            bool sign) {
+  // invert
+  MemoryValue result{onesComplementC(value, size, sign)};
+  // subtract one
+  for (std::size_t i = 0; i < result.getSize() - 1; ++i) {
+    if (result.flip(i)) return result;
+  }
+  return result;
+}
+
+const std::function<bool(const MemoryValue&)> unSignum0{ unSignumA };
+const std::function<bool(const MemoryValue&)> Signum0{ signumA };
+
+const std::function<MemoryValue(const MemoryValue&)> nonsigned1{ nonsignedB };
+const std::function<MemoryValue(const MemoryValue&)> signBit1{ signBitB };
+const std::function<MemoryValue(const MemoryValue&)> onesComplement1{ onesComplementB };
+const std::function<MemoryValue(const MemoryValue&)> twosComplement1{ twosComplementB };
+
+const std::function<MemoryValue(const std::vector<std::uint8_t>&, std::size_t, bool)> nonsigned2{ nonsignedC };
+const std::function<MemoryValue(const std::vector<std::uint8_t>&, std::size_t, bool)> signBit2{ signBitC };
+const std::function<MemoryValue(const std::vector<std::uint8_t>&, std::size_t, bool)> onesComplement2{ onesComplementC };
+const std::function<MemoryValue(const std::vector<std::uint8_t>&, std::size_t, bool)> twosComplement2{ twosComplementC };
