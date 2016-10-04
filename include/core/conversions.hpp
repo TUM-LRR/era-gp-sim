bec1d6e2a764d42b8b4f3aa666a40d00e518436e
Riedel Daniel <danield.riedel@tum.de>
2016-10-04 17:48:18 +0200

    added documentation to the conversions

--- include/core/conversions.hpp
+++ include/core/conversions.hpp
@@ -47,11 +47,14 @@ enum class SignedRepresentation {
   ONES_COMPLEMENT,
   SIGN_BIT,
   TWOS_COMPLEMENT,
-  SMART
+  SMART//Smart uses UNSIGNED/TWOS_COMPLEMENT depending whether the type is signed or unsigned
 };
 
+//function to detemine the sign of a MemoryValue
 using signFunction = std::function<bool(const MemoryValue&)>;
+//function to convert an Integral Type to a MemoryValue
 using toMemoryValueFunction = std::function<MemoryValue(const std::vector<std::uint8_t>&, std::size_t, bool)>;
+//function to convert a MemoryValue to an Integral Type
 using toIntegralFunction = std::function<MemoryValue(const MemoryValue&, bool)>;
 
 extern const signFunction unSignum0;
@@ -67,6 +70,7 @@ extern const toMemoryValueFunction signBit2;
 extern const toMemoryValueFunction onesComplement2;
 extern const toMemoryValueFunction twosComplement2;
 
+//wrapper containing a signFunction, toIntegralFunction and toMemoryValueFunction
 struct Conversion {
   signFunction sgn;
   toMemoryValueFunction toMem;
@@ -80,15 +84,21 @@ struct Conversion {
   ~Conversion() = default;
 };
 
+//
 extern const Conversion nonsigned;
 extern const Conversion signBit;
 extern const Conversion onesComplement;
 extern const Conversion twosComplement;
 
-// Unsigned and little endian
+/**
+ * \brief Converts an unsigned MemoryValue (little endian) into integral form
+ * \param memoryValue The tobe converted MemoryValue
+ * \tparam T The desired type of the output
+ * \returns integral representation of memoryValue
+ */
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value, T>::type
-convertLE(const MemoryValue& memoryValue) {
+convertUnsigned(const MemoryValue& memoryValue) {
   std::vector<std::uint8_t> raw{ memoryValue.internal() };
   //TODO::use fancy bitmask stuff and remove dependency on getByteAt
   //const std::uint8_t mask{ 0xFF };
@@ -103,27 +113,62 @@ convertLE(const MemoryValue& memoryValue) {
   return result;
 }
 
-// little endian
-// true => negative
+/**
+ * \brief Converts a MemoryValue (little endian) into integral form
+ * \param memoryValue The to be converted MemoryValue
+ * \param sgn a signFunction to determine the sign of the output
+ * \param abs a toIntegralFunction converting a signed MemoryValue into it's absolute value
+ * \tparam T The desired type of the output
+ * \returns integral representation of memoryValue
+ */
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value, T>::type
 convert(const MemoryValue& memoryValue, const signFunction &sgn, const toIntegralFunction &abs) {
-  bool sign = sgn(memoryValue);
-  T result = convertLE<T>(abs(memoryValue, sign));
+  bool sign = sgn(memoryValue);// true => negative
+  T result = convertUnsigned<T>(abs(memoryValue, sign));
   if (sign ^ (result < 0)) return 0 - result;
   return result;
 }
 
+/**
+ * \brief Converts a MemoryValue (little endian) into integral form
+ * \param memoryValue The to be converted MemoryValue
+ * \param con the method of storing the value
+ * \tparam T The desired type of the output
+ * \returns integral representation of memoryValue
+ */
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value, T>::type
 convert(const MemoryValue& memoryValue, const Conversion &con) {
   return convert<T>(memoryValue,con.sgn,con.toInt);
 }
 
+/**
+ * \brief permutes a memoryValue
+ * \param memoryValue The to be permuted MemoryValue
+ * \param byteSize size of a Byte in bit
+ * \param permutation a permutation of the integers [0;memoryValue.getSize()/byteSize[ mapping resulting index -> origin index
+ * \returns permuted MemoryValue
+ */
 MemoryValue permute(const MemoryValue& memoryValue, const std::size_t byteSize, const std::vector<std::size_t> permutation);
 
+/**
+ * \brief permutes a memoryValue
+ * \param memoryValue The to be permuted MemoryValue
+ * \param byteSize size of a Byte in bit
+ * \param permutation a permutation of the integers [0;memoryValue.getSize()/byteSize[ mapping resulting index -> origin index
+ * \returns permuted MemoryValue
+ */
 MemoryValue permute(const MemoryValue& memoryValue, const std::size_t byteSize, const std::function<std::size_t(std::size_t)> permutation);
 
+/**
+ * \brief Converts an integral value into a MemoryValue (little endian)
+ * \param value The to be converted value
+ * \param sgn function storing the sign
+ * \param size number of bits to reserve for storing the value
+ * \tparam T The type of the input
+ * \returns MemoryValue representating value
+ */
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value, MemoryValue>::type
 convert(const T value, const toMemoryValueFunction& sgn, const std::size_t size) {
@@ -139,33 +184,38 @@ convert(const T value, const toMemoryValueFunction& sgn, const std::size_t size)
   return sgn(raw, size, value < 0);
 }
 
+/**
+ * \brief Converts an integral value into a MemoryValue (little endian)
+ * \param value The to be converted value
+ * \param con the method of storing the value
+ * \param size number of bits to reserve for storing the value
+ * \tparam T The type of the input
+ * \returns MemoryValue representating value
+ */
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value, MemoryValue>::type
 convert(T value, const Conversion con, std::size_t size) {
   return convert<T>(value, con.toMem, size);
 }
 
+//signFunction
 bool unSignumA(const MemoryValue& memoryValue);
-
 bool signumA(const MemoryValue& memoryValue);
 
+//toIntegralFunction
 MemoryValue nonsignedB(const MemoryValue& memoryValue, bool sign);
-
 MemoryValue signBitB(const MemoryValue& memoryValue, bool sign);
-
 MemoryValue onesComplementB(const MemoryValue& memoryValue, bool sign);
-
 MemoryValue twosComplementB(const MemoryValue& memoryValue, bool sign);
 
+//toMemoryValueFunction
 MemoryValue nonsignedC(const std::vector<std::uint8_t>& value, std::size_t size, bool sign);
-
 MemoryValue signBitC(const std::vector<std::uint8_t>& value, std::size_t size, bool sign);
-
 MemoryValue onesComplementC(const std::vector<std::uint8_t>& value, std::size_t size, bool sign);
-
 MemoryValue twosComplementC(const std::vector<std::uint8_t>& value, std::size_t size, bool sign);
 
 namespace {
+//brief function used as BigEndian permutation
 struct bigEndian {
   std::size_t _byteCount;
   bigEndian(std::size_t byteCount) :_byteCount{ byteCount } {}
@@ -180,9 +230,31 @@ struct bigEndian {
 };
 }
 
+/**
+ * \brief maps SignedRepresentation -> conversion
+ * \param representation the method of conversion
+ * \returns a methid of conversion
+ */
 Conversion switchConversion(SignedRepresentation representation);
 
+/**
+ * \brief permutes a memoryValue
+ * \param memoryValue The to be permuted MemoryValue
+ * \param byteOrder enum determining the method of conversion
+ * \param byteSize size of a Byte in bit
+ * \returns permuted MemoryValue
+ */
 MemoryValue permute(const MemoryValue& memoryValue, Endianness byteOrder, std::size_t byteSize);
+
+/**
+ * \brief Converts a MemoryValue into integral form
+ * \param memoryValue The to be converted MemoryValue
+ * \param representation the method of storing the value
+ * \param byteSize soze of a byte in bit
+ * \param byteOrder Endianess of the output
+ * \tparam T The desired type of the output
+ * \returns integral representation of memoryValue
+ */
 
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value, T>::type
@@ -195,14 +267,24 @@ convert(const MemoryValue& memoryValue, SignedRepresentation representation=Sign
   return convert<T>(permuted,con);
 }
 
+/**
+ * \brief Converts an integral value into a MemoryValue
+ * \param value The to be converted value
+ * \param size number of bits to reserve for storing the value
+ * \param representation the method of storing the value
+ * \param byteSize size of a byte in bit
+ * \param byteOrder Endianess of the output
+ * \tparam T The type of the input
+ * \returns MemoryValue representating value
+ */
 template <typename T>
 typename std::enable_if<std::is_integral<T>::value, MemoryValue>::type
-convert(T value, SignedRepresentation representation=SignedRepresentation::SMART, std::size_t byteSize=8, Endianness byteOrder=Endianness::LITTLE) {
+convert(T value, std::size_t size, SignedRepresentation representation=SignedRepresentation::SMART, std::size_t byteSize=8, Endianness byteOrder=Endianness::LITTLE) {
   Conversion con = switchConversion(representation);
   if (representation == SignedRepresentation::SMART && std::is_signed<T>::value) {
     con = twosComplement;
   }
-  MemoryValue converted = convert<T>(value, con, byteSize);
+  MemoryValue converted = convert<T>(value, con, size);
   return permute(converted, byteOrder, byteSize);
 }
 
