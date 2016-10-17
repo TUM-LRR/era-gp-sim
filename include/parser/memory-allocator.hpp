08ea466a4b4df2515075fe37362602d6f558be00
DEXIS <david.schneller@tum.de>
2016-10-17 19:06:17 +0200

    Updating memory allocator interface. #106

--- include/parser/memory-allocator.hpp
+++ include/parser/memory-allocator.hpp
@@ -30,17 +30,9 @@ struct RelativeMemoryPosition {
     std::string section;
     std::size_t offset;
 
-    RelativeMemoryPosition(const std::string& section, std::size_t offset)
-    : section(section), offset(offset)
-    {
-        
-    }
+    RelativeMemoryPosition(const std::string& section, std::size_t offset);
 
-    RelativeMemoryPosition()
-    : RelativeMemoryPosition("", 0)
-    {
-
-    }
+    RelativeMemoryPosition();
 };
 
 struct MemorySectionDefinition {
@@ -48,25 +40,11 @@ struct MemorySectionDefinition {
     std::size_t sectionAlignment;
     std::size_t dataAlignment;
 
-    MemorySectionDefinition(const std::string& name, std::size_t sectionAlignment, std::size_t dataAlignment)
-        : name(name), sectionAlignment(sectionAlignment), dataAlignment(dataAlignment)
-    {
-        assert::that(sectionAlignment > 0);
-        assert::that(dataAlignment > 0);
-        assert::that(sectionAlignment % dataAlignment == 0);
-    }
+    MemorySectionDefinition(const std::string& name, std::size_t sectionAlignment, std::size_t dataAlignment);
 
-    MemorySectionDefinition(const std::string& name, std::size_t alignment)
-        : MemorySectionDefinition(name, alignment, alignment)
-    {
+    MemorySectionDefinition(const std::string& name, std::size_t alignment);
 
-    }
-
-    MemorySectionDefinition(const std::string& name)
-        : MemorySectionDefinition(name, 1, 1)
-    {
-
-    }
+    MemorySectionDefinition(const std::string& name);
 };
 
 class MemoryAllocator {
@@ -75,100 +53,35 @@ public:
     public:
         friend MemoryAllocator;
 
-        void clear()
-        {
-            _currentSize = 0;
-        }
+        MemorySection(const MemorySectionDefinition& definition);
 
-        std::size_t allocateRelative(std::size_t size)
-        {
-            auto aligned = Utility::discreteCeiling(_currentPosition, _definition.dataAlignment) * _definition.dataAlignment;
-            _currentSize = aligned + size;
-            return aligned;
-        }
+        void clear();
 
-        std::size_t currentSize() const
-        {
-            return _currentSize;
-        }
+        std::size_t allocateRelative(std::size_t size);
 
-        std::size_t currentPosition() const
-        {
-            return _currentPosition;
-        }
+        std::size_t currentSize() const;
 
-        std::size_t absoluteAddress(std::size_t relative) const
-        {
-            return relative + _currentPosition;
-        }
+        std::size_t currentPosition() const;
+
+        std::size_t absoluteAddress(std::size_t relative) const;
 
     private:
-        MemorySection(const MemorySectionDefinition& definition)
-            : _definition(definition), _currentSize(0), _currentPosition(0)
-        {
-            
-        }
-
-        /*MemorySection(const MemorySection& other) = default;
-        MemorySection(MemorySection&& other) = default;
-        MemorySection& operator = (const MemorySection& other) = default;
-        MemorySection& operator = (MemorySection&& other) = default;
-        ~MemorySection() = default;*/
-
         MemorySectionDefinition _definition;
         std::size_t _currentSize;
         std::size_t _currentPosition;
     };
 
-    MemoryAllocator(const std::vector<MemorySectionDefinition>& sectionDefinitions)
-    {
-        _sections.reserve(sectionDefinitions.size());
-        for (const auto& i : sectionDefinitions)
-        {
-            auto section = MemorySection(i);
-            _nameToSection[i.name] = _sections.size();
-            _sections.push_back(section);
-        }
-    }
+    MemoryAllocator(const std::vector<MemorySectionDefinition>& sectionDefinitions);
 
-    void clear()
-    {
-        for (auto& i : _sections)
-        {
-            i.clear();
-        }
-    }
+    void clear();
 
-    std::size_t calculatePositions()
-    {
-        std::size_t position = 0;
-        for (auto& i : _sections)
-        {
-            auto sectionAlign = i._definition.sectionAlignment;
-            auto aligned = Utility::discreteCeiling(position, sectionAlign) * sectionAlign;
-            i._currentPosition = aligned;
-            position = aligned + i._currentSize;
-        }
-        return position;
-    }
+    std::size_t calculatePositions();
 
-    MemorySection& operator[](std::string name) 
-    {
-        assert::that(_nameToSection.find(name) != _nameToSection.end());
-        return _sections[_nameToSection[name]]; 
-    }
+    MemorySection& operator[](std::string name);
 
-    MemorySection& operator[](std::size_t index)
-    {
-        assert::that(index >= 0);
-        assert::that(index < _sections.size());
-        return _sections[index];
-    }
+    MemorySection& operator[](std::size_t index);
 
-    std::size_t absolutePosition(const RelativeMemoryPosition& relative) const
-    {
-        return _sections.at(_nameToSection.at(relative.section)).absoluteAddress(relative.offset);
-    }
+    std::size_t absolutePosition(const RelativeMemoryPosition& relative) const;
 private:
     std::vector<MemorySection> _sections;
     std::unordered_map<std::string, std::size_t> _nameToSection;
--- source/parser/memory-allocator.cpp
+++ source/parser/memory-allocator.cpp
@@ -18,3 +18,118 @@
 */
 
 #include "parser\memory-allocator.hpp"
+
+RelativeMemoryPosition::RelativeMemoryPosition(const std::string& section, std::size_t offset)
+    : section(section), offset(offset)
+    {
+        
+    }
+
+RelativeMemoryPosition::RelativeMemoryPosition()
+    : RelativeMemoryPosition("", 0)
+    {
+
+    }
+
+MemorySectionDefinition::MemorySectionDefinition(const std::string& name, std::size_t sectionAlignment, std::size_t dataAlignment)
+        : name(name), sectionAlignment(sectionAlignment), dataAlignment(dataAlignment)
+    {
+        assert::that(sectionAlignment > 0);
+        assert::that(dataAlignment > 0);
+        assert::that(sectionAlignment % dataAlignment == 0);
+    }
+
+    MemorySectionDefinition::MemorySectionDefinition(const std::string& name, std::size_t alignment)
+        : MemorySectionDefinition(name, alignment, alignment)
+    {
+
+    }
+
+    MemorySectionDefinition::MemorySectionDefinition(const std::string& name)
+        : MemorySectionDefinition(name, 1, 1)
+    {
+
+    }
+
+    void MemoryAllocator::MemorySection::clear()
+        {
+            _currentSize = 0;
+        }
+
+        std::size_t MemoryAllocator::MemorySection::allocateRelative(std::size_t size)
+        {
+            auto aligned = Utility::discreteCeiling(_currentPosition, _definition.dataAlignment) * _definition.dataAlignment;
+            _currentSize = aligned + size;
+            return aligned;
+        }
+
+        std::size_t MemoryAllocator::MemorySection::currentSize() const
+        {
+            return _currentSize;
+        }
+
+        std::size_t MemoryAllocator::MemorySection::currentPosition() const
+        {
+            return _currentPosition;
+        }
+
+        std::size_t MemoryAllocator::MemorySection::absoluteAddress(std::size_t relative) const
+        {
+            return relative + _currentPosition;
+        }
+
+        MemoryAllocator::MemorySection::MemorySection(const MemorySectionDefinition& definition)
+            : _definition(definition), _currentSize(0), _currentPosition(0)
+        {
+            
+        }
+
+        MemoryAllocator::MemoryAllocator(const std::vector<MemorySectionDefinition>& sectionDefinitions)
+    {
+        _sections.reserve(sectionDefinitions.size());
+        for (const auto& i : sectionDefinitions)
+        {
+            auto section = MemorySection(i);
+            _nameToSection[i.name] = _sections.size();
+            _sections.push_back(section);
+        }
+    }
+    
+    void MemoryAllocator::clear()
+    {
+        for (auto& i : _sections)
+        {
+            i.clear();
+        }
+    }
+
+    std::size_t MemoryAllocator::calculatePositions()
+    {
+        std::size_t position = 0;
+        for (auto& i : _sections)
+        {
+            auto sectionAlign = i._definition.sectionAlignment;
+            auto aligned = Utility::discreteCeiling(position, sectionAlign) * sectionAlign;
+            i._currentPosition = aligned;
+            position = aligned + i._currentSize;
+        }
+        return position;
+    }
+
+    MemorySection& MemoryAllocator::operator[](std::string name) 
+    {
+        assert::that(_nameToSection.find(name) != _nameToSection.end());
+        return _sections[_nameToSection[name]]; 
+    }
+
+    MemorySection& MemoryAllocator::operator[](std::size_t index)
+    {
+        assert::that(index >= 0);
+        assert::that(index < _sections.size());
+        return _sections[index];
+    }
+
+    std::size_t MemoryAllocator::absolutePosition(const RelativeMemoryPosition& relative) const
+    {
+        return _sections.at(_nameToSection.at(relative.section)).absoluteAddress(relative.offset);
+    }
