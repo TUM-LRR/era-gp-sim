9b430f27a3964f115674022ad9aa40b7a1356018
DEXIS <david.schneller@tum.de>
2016-10-19 16:09:07 +0200

    Fixed a slash. #53

--- source/parser/riscv-directives.cpp
+++ source/parser/riscv-directives.cpp
@@ -17,7 +17,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "parser\riscv-directives.hpp"
+#include "parser/riscv-directives.hpp"
 
 MemoryReservationDirective::ArgumentCompileFunction
     RiscvDirectives::_argumentCompile =
