bb3f6c068a8250b585ec46f0458176b6e4b028b6
goldsborough <peter@goldsborough.me>
2016-12-14 00:37:19 +0000

    Replaced all <gtest/gtest.h> includes with 'gtest/gtest.h'

--- tests/arch/riscv/assemble-test.cpp
+++ tests/arch/riscv/assemble-test.cpp
@@ -15,7 +15,7 @@
  * You should have received a copy of the GNU General Public License
 * along with this program. If not, see <http://www.gnu.org/licenses/>.*/
 
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 
 #include "arch/riscv/properties.hpp"
 #include "arch/riscv/utility.hpp"
--- tests/common/common-utility-tests.cpp
+++ tests/common/common-utility-tests.cpp
@@ -17,10 +17,10 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <gtest/gtest.h>
 #include <limits>
 #include <string>
 #include <vector>
+#include "gtest/gtest.h"
 
 #include "common/utility.hpp"
 #include "core/conversions.hpp"
