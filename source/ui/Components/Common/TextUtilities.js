2a4178f2c880b8196503b46c1a4dfc5550cd966e
Felix Opolka <opolkafelix@gmail.com>
2016-12-21 17:24:56 +0100

    Fixed variable bug
    #161
    * Fixed wrong variable

--- source/ui/Components/Common/TextUtilities.js
+++ source/ui/Components/Common/TextUtilities.js
@@ -2,15 +2,15 @@
 function getLineEndForLine(text, lineNumber) {
     var position = getPositionOfOccurence(text, "\n", lineNumber+1);
     // Is last line.
-    if (position === 0) { position = text.length; }
+    if (position === 0) position = text.length;
     return getLineEndForPosition(text, position);
 }
 
 // Returns the position of the end of the line (i.e. the next character would be the newline character)
 // relative to the entire text.
 function getLineEndForPosition(text, position) {
-    var lineEnd = textArea.text.slice(position).indexOf("\n") + position;
-    if (lineEnd == -1) { lineEnd = text.length; }
+    var lineEnd = text.slice(position).indexOf("\n") + position;
+    if (lineEnd == -1) lineEnd = text.length;
     return lineEnd;
 }
 
