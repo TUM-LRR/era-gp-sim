fb38e53e03553556a2bb061ff976f137f9c0d78e
DEXIS <david.schneller@tum.de>
2016-12-26 16:01:13 +0100

    Fixing a Linux-specific bug. #168 #77

--- source/parser/independent/transformation-parameters.cpp
+++ source/parser/independent/transformation-parameters.cpp
@@ -17,7 +17,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include "parser/independent\transformation-parameters.hpp"
+#include "parser/independent/transformation-parameters.hpp"
 
 TransformationParameters::TransformationParameters(
     const Architecture& architecture,
