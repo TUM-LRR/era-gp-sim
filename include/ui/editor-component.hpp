74336043fc4fd6f67cc5baa8826127416da3bac7
Felix Opolka <opolkafelix@gmail.com>
2016-12-29 20:48:12 +0100

    Added const qualifier
    #161

--- include/ui/editor-component.hpp
+++ include/ui/editor-component.hpp
@@ -153,7 +153,7 @@ class EditorComponent : public QObject {
   void deleteErrors();
 
   /** A signal to add an error in the editor. */
-  void addIssue(QString message, int line, QString color);
+  void addIssue(const QString &message, int line, const QString &color);
 
   /** Change the highlighted line which indicates the execution point. */
   void executionLineChanged(int line);
