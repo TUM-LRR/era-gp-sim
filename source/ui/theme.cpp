a6b0ae2596fbc69da24b621592e7e17f9a3b6f05
goldsborough <peter@goldsborough.me>
2017-02-02 21:25:29 +0000

    Stylistic changes

--- source/ui/theme.cpp
+++ source/ui/theme.cpp
@@ -28,9 +28,9 @@
 #include "common/assert.hpp"
 #include "common/utility.hpp"
 
-Theme *Theme::_theme = nullptr;
+Theme* Theme::_theme = nullptr;
 
-Status Theme::Make(const QString &themeName) {
+Status Theme::Make(const QString& themeName) {
   assert::that(!themeName.isEmpty());
   assert::that(_theme == nullptr);
 
@@ -46,30 +46,30 @@ Status Theme::Make(const QString &themeName) {
   return status;
 }
 
-Theme &Theme::instance() {
+Theme& Theme::instance() {
   assert::that(_theme != nullptr);
   return *_theme;
 }
 
-Theme *Theme::pointer() {
+Theme* Theme::pointer() {
   assert::that(_theme != nullptr);
   return _theme;
 }
 
-Theme::Theme() : super(this, nullptr) {}
+Theme::Theme() : super(this, nullptr) {
+}
 
-Status Theme::load(const QString &themeName) {
+Status Theme::load(const QString& themeName) {
   assert::that(!themeName.isEmpty());
 
   auto result = _loadJson(themeName);
-  if (!result)
-    return result.status();
+  if (!result) return result.status();
 
-  const auto &json = result.value();
+  const auto& json = result.value();
 
   // We simply override all entries
   for (auto iterator = json.begin(); iterator != json.end(); ++iterator) {
-    const auto &value = iterator.value();
+    const auto& value = iterator.value();
     assert::that(value.isObject());
     super::insert(iterator.key(), value.toObject().toVariantMap());
   }
@@ -84,8 +84,8 @@ const QString Theme::currentThemeName() const noexcept {
   return _currentThemeName;
 }
 
-StatusWithValue<QByteArray> Theme::_loadThemeData(const QString &name) {
-  auto directory = QDir::home(); // Represents an absolute path to $HOME.
+StatusWithValue<QByteArray> Theme::_loadThemeData(const QString& name) {
+  auto directory = QDir::home();  // Represents an absolute path to $HOME.
 
   if (!directory.exists()) {
     return Status::Fail("Could not find home directory");
@@ -115,15 +115,14 @@ StatusWithValue<QByteArray> Theme::_loadThemeData(const QString &name) {
   return contents;
 }
 
-StatusWithValue<const Theme::Json &> Theme::_loadJson(const QString &name) {
+StatusWithValue<const Theme::Json&> Theme::_loadJson(const QString& name) {
   auto iterator = _cache.constFind(name);
   if (iterator != _cache.cend()) {
     return iterator.value();
   }
 
   auto data = _loadThemeData(name);
-  if (!data)
-    return data.status();
+  if (!data) return data.status();
 
   auto json = QJsonDocument::fromJson(data.value());
 
@@ -133,8 +132,8 @@ StatusWithValue<const Theme::Json &> Theme::_loadJson(const QString &name) {
   return _cacheTheme(name, object);
 }
 
-const Theme::Json &Theme::_cacheTheme(const QString &name,
-                                      QJsonObject &object) {
+const Theme::Json&
+Theme::_cacheTheme(const QString& name, QJsonObject& object) {
   // Pop a "random" element (random because the container is unordered)
   if (_cache.size() == CACHE_CAPACITY) {
     _cache.erase(_cache.begin());
